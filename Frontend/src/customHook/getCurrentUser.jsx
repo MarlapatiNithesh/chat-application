6f038435b7cd58a6979d957cbfe938ec7ada6119
Marlapati Nitheesh <marlapati.2023ug1112@iiitranchi.ac.in>
2025-05-19 08:39:20 +0530

    redirects

--- Frontend/src/customHook/getCurrentUser.jsx
+++ Frontend/src/customHook/getCurrentUser.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import { setUserData, clearUserData, setLoading } from "../redux/userSlice";
+import { setUserData, clearUserData } from "../redux/userSlice";
 import { serverUrl } from "../main";
+
 
 const useGetCurrentUser = () => {
   const dispatch = useDispatch();
@@ -10,17 +11,15 @@ const useGetCurrentUser = () => {
 
   useEffect(() => {
     const fetchUser = async () => {
-      dispatch(setLoading(true)); // start loading
       try {
-        const response = await axios.get(`${serverUrl}/api/user/current`, {
-          withCredentials: true,
-        });
+        const response = await axios.get(
+          `${serverUrl}/api/user/current`,
+          { withCredentials: true }
+        );
         dispatch(setUserData(response.data));
       } catch (error) {
-        dispatch(clearUserData()); // reset user
+        dispatch(clearUserData()); // reset user & stop loading
         console.error("Error fetching current user:", error);
-      } finally {
-        dispatch(setLoading(false)); // stop loading
       }
     };
 
