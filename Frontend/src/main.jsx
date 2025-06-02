3f74ae9c44ed2f91d9beaaace05273b211218bdd
Marlapati Nitheesh <marlapati.2023ug1112@iiitranchi.ac.in>
2025-06-02 20:07:03 +0530

    updated

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,16 +1,20 @@
-
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+
+import { store, persistor } from "./redux/store.js"; // note persistor import
+import { PersistGate } from "redux-persist/integration/react";
 
 export const serverUrl = "http://65.0.97.103:3001";
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -2,16 +2,47 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
 import messageSlice from "./messageSlice";
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
+const userPersistConfig = {
+  key: "user",
+  storage,
+  whitelist: ["userData", "otherUsers", "selectedUser", "onlineUsers", "unreadCounts", "lastActivity"], // only persist these keys in user slice state
+};
+
+const messagePersistConfig = {
+  key: "message",
+  storage,
+  whitelist: ["messages"], // adjust according to your messageSlice state keys
+};
+
+const persistedUserReducer = persistReducer(userPersistConfig, userSlice);
+const persistedMessageReducer = persistReducer(messagePersistConfig, messageSlice);
+
 export const store = configureStore({
   reducer: {
-    user: userSlice,
-    message: messageSlice,
+    user: persistedUserReducer,
+    message: persistedMessageReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: ["user/setSocket"],
+        // redux-persist actions you want to ignore for serializableCheck
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, "user/setSocket"],
         ignoredPaths: ["user.socket"],
       },
     }),
 });
+
+export const persistor = persistStore(store);
